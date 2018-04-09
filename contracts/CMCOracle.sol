3f1a3c1c8008f194bf62fd145b8aa7749a47d831
Andy Thomas <taff@Taff-MacBook-Pro-2.local>
2018-04-09 09:39:43 +0100

    Fixed syntax error this.balance changed to address(this).balance

--- contracts/CMCOracle.sol
+++ contracts/CMCOracle.sol
@@ -1,55 +1,46 @@
-pragma solidity ^0.4.19;
+pragma solidity ^0.4.21;
 
 import "./ownable.sol";
 
 
-contract CMCOracle {
-    // Contract owner
-    address owner;
+contract CMCOracle is Ownable {
     
     // BTC Marketcap Storage
     uint private marketCap;
     uint256 public oracleFee;
     
     mapping (address => uint) public purchasers;
+    
+    // Callback function
+    event CallbackMarketCap();
 
     function CMCOracle() public {
         owner = msg.sender;
     }
     
-    // Callback function
-    event CallbackMarketCap();
-
     function () public payable {
     } 
-
-    function updateMarketCap() public payable {
         
+    function setMarketCap(uint cap) external onlyOwner {
+        marketCap = cap;
+    }
+    
+    function withdrawEther() external onlyOwner {
+        msg.sender.transfer(address(this).balance);
+    }
+
+    function setOracleFee(uint256 fee) external onlyOwner {
+        oracleFee = fee;
+    }
+    
+    function updateMarketCap() public payable {        
         if (msg.value < oracleFee) return;     
         // Calls the callback function
         purchasers[msg.sender]++; 
-        CallbackMarketCap();              
+        emit CallbackMarketCap();              
     }
-
-    function setMarketCap(uint cap) public {
-        // If it isn't sent by a trusted oracle
-        // a.k.a ourselves, ignore it
-        require(msg.sender == owner);
-        marketCap = cap;
-    }
-
+    
     function getBalance() public view returns (uint256) {
         return address(this).balance;
-    }
-
-    function setOracleFee(uint256 fee) public {
-        // If it isn't sent by a trusted oracle a.k.a ourselves, ignore it
-        require(msg.sender == owner);
-        oracleFee = fee;
-    }
-
-    function withdrawEther() public {
-        require(msg.sender == owner);
-        msg.sender.transfer(this.balance);
     }
 }
